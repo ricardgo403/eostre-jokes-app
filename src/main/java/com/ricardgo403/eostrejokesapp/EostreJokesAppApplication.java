c232cc66441f287c79fc3b6691a774365d037520
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-19 13:08:22 -0500

    Feat: Initial commit. Initialize Springboot project. Closes #1

---
+++ src/main/java/com/ricardgo403/eostrejokesapp/EostreJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.ricardgo403.eostrejokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EostreJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EostreJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardgo403/eostrejokesapp/EostreJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardgo403.eostrejokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EostreJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
